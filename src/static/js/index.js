6a03085cc2dd6b9f3cd047aea8993e3107c9bf54
Pau Argelaguet <pau@outlook.com>
2017-12-23 10:50:19 +0100

    Moving to Firefox again

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -44,10 +44,17 @@ function handleLogin(data) {
 
             //using Google public stun server
             var configuration = {
-                "iceServers": [{"urls": "stun:stun2.1.google.com:19302"}]
+                "iceServers": [{
+                    "urls": [
+                        "stun:stun.l.google.com:19302",
+                        "stun:stun1.l.google.com:19302",
+                        "stun:stun2.l.google.com:19302",
+                        "stun:stun3.l.google.com:19302",
+                        "stun:stun4.l.google.com:19302"]
+                }]
             };
 
-            yourConn = new webkitRTCPeerConnection(configuration);
+            yourConn = new RTCPeerConnection(configuration);
 
             // setup stream listening
             yourConn.addStream(stream);
