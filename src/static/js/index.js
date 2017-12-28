fe5aa7961a9b17aa28991e2215fd7df1584691af
Pau Argelaguet <pau@outlook.com>
2017-12-28 17:13:55 +0100

    Working on chat UI

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -170,11 +170,7 @@ function handleCandidate(candidate) {
 
 function handleLeave() {
     targetUsername = null;
-
-    if (remoteVideo.srcObject) {
-        remoteVideo.srcObject.getTracks().forEach(track => track.stop());
-    }
-    remoteVideo.src = null;
+    remoteVideo.srcObject.getTracks().forEach(track => track.stop());
 
     myPeerConn.close();
     myPeerConn.onicecandidate = null;
@@ -246,9 +242,9 @@ function call(callToUsername) {
 }
 
 $(document.body).on('click', '#hangUpBtn', function (e) {
-    /*send({
+    send({
         type: "leave"
-    });*/
+    });
     handleLeave();
 });
 
@@ -286,3 +282,50 @@ $(document).ready(function () {
     showStopWatch();
     $('#modalUsers').modal('show');
 });
+
+$(".feed").scrollTop($(".feed")[0].scrollHeight);
+document.getElementById("message").addEventListener('keypress', function (e) {
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var key = e.which || e.keyCode;
+    if (key == 13) { // 13 is enter
+        var message = this.value;
+        $('.feed').append("<div class='me'><div class='message'>" + (this.value) + "<div class='meta'>11/19/13, " + hours + ":" + minutes + " PM</div></div></div>");
+        $(".feed").scrollTop($(".feed")[0].scrollHeight);
+        this.value = "";
+    }
+});
+$('#chathead').click(function () {
+    $('#togglearea').slideToggle();
+});
+(function ($) {
+    $(document).ready(function () {
+        $('[data-corin-checkbox="true"]')
+            .addClass('corin-checkbox')
+            .wrap('<div class="corin-checkbox-container"></div>')
+            .after('<div class="corin-checkbox-sub"></div>')
+            .each(function () {
+                if (this.checked) {
+                    $(this).siblings('.corin-checkbox-sub').addClass('checked');
+                } else {
+                    $(this).siblings('.corin-checkbox-sub').addClass('unchecked');
+                }
+            })
+            .parent()
+            .on('click', '.corin-checkbox-sub', function () {
+                var theCheckbox = $(this).siblings('.corin-checkbox');
+                var isChecked = theCheckbox.is(':checked');
+
+                if (isChecked) {
+                    theCheckbox.prop('checked', false);
+                    $(this).removeClass('checked').addClass('unchecked');
+                } else {
+                    theCheckbox.prop('checked', true);
+                    $(this).removeClass('unchecked').addClass('checked');
+                }
+
+            });
+    });
+
+}(jQuery));
