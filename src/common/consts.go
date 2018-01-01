7ebe3581861d9925fb3e1b7a4bf6e740110ff419
Pau Argelaguet <pau@outlook.com>
2018-01-01 20:14:16 +0100

    Less network congestion

--- src/common/consts.go
+++ src/common/consts.go
@@ -1,9 +1,9 @@
 package common
 
 const BufferSize = 8192
-const DefaultRTimer = 5
+const DefaultRTimer = 20
 const GossipPort = 5000
 const GuiPort = 8080
-const LongTimeOutSecs = 5
+const LongTimeOutSecs = 20
 const MaxHops = 25
 const TimeOutSecs = 1
