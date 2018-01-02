d555455203b776f0d8c0de98178ca982385ef625
Pau Argelaguet <pau@outlook.com>
2018-01-02 15:22:38 +0100

    Adding TURN server

--- src/static/js/functions.js
+++ src/static/js/functions.js
@@ -125,12 +125,11 @@ function initMedia(callback) {
                         urls: 'stun:159.29.27.97:3478'
                         //urls: 'stun:stun.l.google.com:19302'
                     },
-
-                    /*{
+                    {
                         urls: "turn:159.29.27.97:3479",
                         username: "gossip",
                         credential: "gossip"
-                    }*/
+                    }
                 ]
             };
 
