249edb754e328520ba0c438644af809d0b2168df
Pau Argelaguet <pau@outlook.com>
2018-01-04 01:00:41 +0100

    Updated polling times

--- src/common/consts.go
+++ src/common/consts.go
@@ -1,9 +1,9 @@
 package common
 
 const BufferSize = 8192
-const DefaultRTimer = 30
+const DefaultRTimer = 20
 const GossipPort = 5000
 const GuiPort = 8080
-const LongTimeOutSecs = 30
+const LongTimeOutSecs = 20
 const MaxHops = 25
 const TimeOutSecs = 1
