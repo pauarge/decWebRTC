b8a806743ac2570b7ad0a9d5aa18c08fe4dfe424
Pau Argelaguet <pau@outlook.com>
2018-01-05 19:29:16 +0100

    Less network overload

--- src/common/consts.go
+++ src/common/consts.go
@@ -4,6 +4,6 @@ const BufferSize = 8192
 const DefaultRTimer = 20
 const GossipPort = 5000
 const GuiPort = 8080
-const LongTimeOutSecs = 20
+const LongTimeOutSecs = 30
 const MaxHops = 25
 const TimeOutSecs = 1
